a2f476abd0a76658a52cc61a3fbabfc2f84cf8fa
rmortale <rmortale@gmx.net>
2017-02-11 17:45:28 +0100

    added codefresh

---
+++ src/test/java/com/airhacks/GreetingResourceTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.airhacks;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Nino
+ */
+public class GreetingResourceTest {
+
+    @Test
+    public void testSomeMethod() {
+        assertTrue(true);
+    }
+
+}
